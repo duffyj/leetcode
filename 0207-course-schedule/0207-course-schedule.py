5c9894c753374b72b1395996b98535636b8ec53d
James <james.duffy@gmail.com>
2023-08-12 12:08:35 +0100

    Time: 115 ms (46.73%), Space: 22.4 MB (5.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -12,15 +12,15 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 
-        schedule,visited = dict(), set()
+        schedule,visited = {i: [] for i in range(numCourses)}, set()
 
         for c,p in prerequisites:
-            schedule[c] = schedule.get(c,[]) + [p]
+            schedule[c].append(p)
         
         def istakeable(c):
             if c not in visited:
                 visited.add(c)
-                schedule[c] = [p for p in schedule.get(c,[]) if not istakeable(p)]
+                schedule[c] = tuple(p for p in schedule.get(c,[]) if not istakeable(p))
             return not schedule[c]
              
         for c in range(numCourses):
@@ -30,3 +30,32 @@ class Solution:
         return True
  
 
+class SolutionLeet:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # dfs
+        preMap = {i: [] for i in range(numCourses)}
+
+        # map each course to : prereq list
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visiting = set()
+
+        def dfs(crs):
+            if crs in visiting:
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
