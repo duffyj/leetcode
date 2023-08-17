5a9ba232b119639ac7c53637a9da812096446c27
James <james.duffy@gmail.com>
2023-08-17 19:42:58 +0100

    Time: 45 ms (51.24%), Space: 16.5 MB (19.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -17,3 +17,29 @@ class Solution:
                 arr[row][col] = arr[row-1][col] + arr[row][col-1]
 
         return arr[m-1][n-1]
+
+    
+class Solutionbad:
+    def uniquePaths(self, m: int, n: int):
+        paths = [([0] * n)[:] for _ in range(m)]
+        paths [m-1][n-1] = 1
+        for r in range(m-1,-1,-1):
+            for c in range(n-1,-1,-1):
+                print((r,c))
+                if c < n-1:
+                    paths[r][c] += paths[r][c+1]
+                if r < m -1:
+                    paths[r][c] += paths[r+1][c]
+        return paths[0][0]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int):
+        paths = [([0] * n)[:] for _ in range(m)]
+        paths [0][0] = 1
+        for r in range(m):
+            for c in range(n):
+                if c > 0:
+                    paths[r][c] += paths[r][c-1]
+                if r >0:
+                    paths[r][c] += paths[r-1][c]
+        return paths[m-1][n-1]
