5abbcda7305984fd5686d70af8388558b7942f1c
James <james.duffy@gmail.com>
2023-08-20 17:32:36 +0100

    Time: 872 ms (79.54%), Space: 27.4 MB (60.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,29 @@
 
 
+
 class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        A=0
+        B=-100000000000000
+        for p in prices:
+            A = max(A,B+p)
+            B = max(B,-p)
+        return A
+
+class SolutionTre2:
+    def maxProfit(self, prices: List[int]) -> int:
+        t = [[0,0][:] for _ in range(len(prices))] # Sell,Buy Case
+        for (p,i) in zip(prices,range(1,len(prices))):
+            print(i)
+            t[i][0] = max(t[i-1][0],t[i-1][1]-p)
+            if i > 1:
+                t[i][1] = max(t[i-1][1],t[i-1][0]+p)
+        return max(t[-1])
+        
+        
+        
+        
+class Solution2:
     def maxProfit(self, prices: List[int]) -> int:
         minS, maxP = prices[0], 0
         for p in prices:
@@ -17,10 +40,6 @@ class SolutioOrifn:
             maxProfit = max(maxProfit,p-currMinimum)
             currMinimum = min(currMinimum,p)
         return maxProfit
-
-
-
-
 
 
 from sys import maxsize
