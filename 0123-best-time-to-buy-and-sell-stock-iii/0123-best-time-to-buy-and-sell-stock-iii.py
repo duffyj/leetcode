3a4ec3e43958f89b2e4be72015c6cc21e7e5db9a
James <james.duffy@gmail.com>
2023-08-22 07:27:08 +0100

    Time: 811 ms (97.81%), Space: 30.6 MB (58.55%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -3,7 +3,7 @@ import sys
 
 minsize = -10000000000000000 # WHY
 
-class Solution:
+class SolutionFormulaNames:
     def maxProfit(self, prices: List[int]) -> int:
             ti10 = 0
             ti20  = 0
@@ -15,3 +15,21 @@ class Solution:
                 ti10 = max(ti10,ti11 + p)
                 ti11 = max(ti11,-p)
             return ti20
+
+        
+import sys
+
+minsize = -10000000000000000 # WHY
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+            afterSell1 = 0
+            afterSell2  = 0
+            afterBuy1 = minsize
+            afterBuy2 = minsize
+            for p in prices:
+                afterSell2 = max(afterSell2,afterBuy2 + p)
+                afterBuy2 = max(afterBuy2,afterSell1 - p)
+                afterSell1 = max(afterSell1,afterBuy1 + p)
+                afterBuy1 = max(afterBuy1,-p)
+            return afterSell2
