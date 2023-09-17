2a9386de104cd55268e509ebb29ef673283636af
James <james.duffy@gmail.com>
2023-09-17 17:56:17 +0100

    Time: 115 ms (93.34%), Space: 18.8 MB (90.60%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,15 @@
 
 from operator import xor
 
+
 class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res = xor(res,n)
+        return res
+
+class SolutionOld:
     def singleNumber(self, nums: List[int]) -> int:
         val = 0
         for i in nums:
