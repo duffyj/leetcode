47b0c6fa2d51c17efee500689dd78fd077a4086d
James <james.duffy@gmail.com>
2023-09-20 20:26:23 +0100

    Time: 125 ms (63.27%), Space: 17.7 MB (46.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,18 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    def missingNumberBits(self, nums: List[int]) -> int:
         res = 0
         for i in range(len(nums)+1):
             res ^= i
         for n in nums:
             res ^=n
         return res
+
+    def missingNumber(self, nums: List[int]) -> int:
+        # Gauss
+        n = len(nums)
+        res = n * (n+1) / 2
+        for i in nums:
+            res -= i
+        return int(res)
+        
         
