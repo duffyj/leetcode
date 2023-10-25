bb03b5706a87d43119e31c25b9fc08369c6c46b4
James <james.duffy@gmail.com>
2023-10-25 07:42:36 +0100

    Time: 69 ms (29.11%), Space: 17.8 MB (11.20%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,4 +1,85 @@
-class Solution:
+class SolutionSimpe:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = -10000000000000
+        profit = 0
+        for n in nums:
+            buy = max (-p,buy)
+            profit = max(n+buy,profit)
+        return profit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SolutionSimpe:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
         for i in range (1,len(prices)):
@@ -6,3 +87,12 @@ class Solution:
                 profit +=  prices[i] - prices[i-1]
         return profit
     
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sold, bought = 0, -10000000000000
+
+        for p in prices:
+            oldSold = sold
+            sold = max(sold,bought+p)
+            bought = max(bought,oldSold-p)
+        return sold
